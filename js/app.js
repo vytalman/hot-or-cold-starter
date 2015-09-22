236bf9f1106fe5342bc61c43da9ac600d571030b
vytalman <ryanmesa@gmail.com>
2015-09-22 02:18:58 -0400

    add global variable to store maximum value to be generated

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 
 $(document).ready(function(){
+	// Global variable to store maximum number to be generated
+	MAXVAL = 100;
+	
 	window.onload = function() {
 		newGame();
 	}
 	
 	function newGame() {
 		// Generate a number between 1 and 100
-		var secretNum = Math.floor((Math.random() * 100) + 1);
+		var secretNum = Math.floor((Math.random() * MAXVAL) + 1);
 		console.log("secretNum = " + secretNum);
+		
+		return secretNum;
 	}
 	
 	// New Game Button
