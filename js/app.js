9e78babe911070aa6f43ba32df9ee0340ccb56e4
vytalman <ryanmesa@gmail.com>
2015-09-22 01:31:56 -0400

    edit newGame function and add onload function to generate number when page loads

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 
 $(document).ready(function(){
+	window.onload = function() {
+		newGame();
+	}
+	
+	function newGame() {
+		// Generate a number between 1 and 100
+		var secretNum = Math.floor((Math.random() * 100) + 1);
+		console.log("secretNum = " + secretNum);
+	}
+	
+	// New Game Button
+	$('.new').click(function() {
+		newGame();
+	});
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
