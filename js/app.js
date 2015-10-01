f3405fb8cf19853e6e7f5257725cc3ed5283303b
vytalman <ryanmesa@gmail.com>
2015-10-01 16:24:00 -0400

    fix check guess button

--- js/app.js
+++ js/app.js
@@ -4,9 +4,8 @@ $(document).ready(function(){
 	var MAXVAL = 100;
 	var SECRETNUM ='';
 	
-	window.onload = function() {
-		newGame();
-	}
+	window.onload = newGame;
+	//newGame();
 	
 	function newGame() {
 		// Generate a number between 1 and 100
@@ -14,14 +13,43 @@ $(document).ready(function(){
 		console.log("SECRETNUM = " + SECRETNUM);
 	}
 	
+	/*function checkGuess() {
+		var guessInput = document.getElementById('userGuess');
+		var guess = guessInput.value;
+		var absGuess = Math.abs(SECRETNUM - guess);
+		
+		if (absGuess >= 50) {
+			// Ice Cold
+			console.log("Ice Cold");
+		} else if (absGuess>=30 && absGuess<50) {
+			// Cold
+			console.log("Cold");
+		} else if (absGuess>=20 && absGuess<30) {
+			// Warm
+			console.log("Warm");
+		} else if (absGuess>=10 && absGuess<20) {
+			// Hot
+			console.log("Hot");
+		} else if (absGuess>=1 && absGuess<10) {
+			// Very Hot
+			console.log("Very Hot");
+		} else if (absGuess == 0) {
+			alert("Congratulations! \nYou guessed the correct number!");
+			newGame();
+		}
+	}*/
+	
 	// New Game Button
 	$('.new').click(function() {
+		console.log("newGame() called...")
 		newGame();
 	});
 	
 	// Guess Button
-	$('#guessButton').click(function() {
-		//console.log("Guess button pushed: SECRETNUM = " + SECRETNUM);
+	$('#guessButton').click(function(event) {
+		event.preventDefault();
+		console.log("Guess Button clicked...");	
+		//checkGuess();
 	});
 	
 	/*--- Display information modal box ---*/
